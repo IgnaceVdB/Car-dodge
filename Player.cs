d351759b8d1bf0a8f6fec07e17e8dfaaa44a734a
ignacedebeste@gmail.com <ignacedebeste@gmail.com>
2019-05-24 11:39:25 +0200

    movement improved, GRANDE SANIC

--- Player.cs
+++ Player.cs
@@ -7,16 +7,16 @@ namespace MyGame
 {
     public class Sanic : Support.Texture
     {
-        public Sanic(Vector2 position) : base("sanic", position, new Vector2(0.2f))
+        public Sanic(Vector2 position) : base("sanic", position, new Vector2(0.4f))
         {
         }
 
         public void Update(GameTime gameTime)
         {
             Vector2 previousPos = Position;
-            if (Keyboard.GetState().IsKeyDown(Keys.Right)) Position.X += 0.1f;
-            if (Keyboard.GetState().IsKeyDown(Keys.Left)) Position.X -= 0.1f;
-           
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) ) Position.X = 0.7f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) ) Position.X = -0.7f;
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) Position.X = 0.0f;
 
 
 
